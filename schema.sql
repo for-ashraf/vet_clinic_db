d181055bd71a00885eaa7fb27fee629e2c028385
for-ashraf <46405183+for-ashraf@users.noreply.github.com>
2022-09-19 20:58:59 +0500

    Index on Vets_id
    Created index on vets_id column of visits table

--- schema.sql
+++ schema.sql
@@ -58,6 +58,8 @@ CREATE TABLE visits (
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
 
 CREATE INDEX idx_ànimal_id ON visits(animal_id);
+CREATE INDEX idx_vets_id ON visits(vets_id);
+
 
 -- End Database Performance Audit:  Day 1:
 
